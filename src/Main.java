9fa78929de73dcb6d78398a2fce07967c4e02cf8
BeGieU <marcinoo890@gmail.com>
2019-03-14 17:37:01 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/*
+Author: BeGieU
+Date: 14.03.2019
+*/
+
+public class Main
+{
+}
---
+++ src/matrix/Matrix.java
@@ -0,0 +1,78 @@
+package matrix;
+/*
+Author: BeGieU
+Date: 14.03.2019
+*/
+
+public class Matrix<T extends Number>
+{
+    private T[][] elementsArr;
+    private int rows;
+    private int columns;
+
+
+    public Matrix(T[][] elementsArr)
+    {
+        if (!isMatrix(elementsArr))
+            throw new IllegalArgumentException("given arr doesnt contain matrix: matrix has to have columns of equal length");
+
+        this.elementsArr = elementsArr;
+        columns = elementsArr.length;
+        rows = elementsArr[0].length;
+    }
+
+    private boolean isMatrix(T[][] elementsArr)
+    {
+        //checks whether matrix has proper size (has to be rectangle)
+        int rowLength = elementsArr[0].length;
+        for (int i = 1; i < elementsArr.length; i++)
+        {
+            //if size is wrong returns false
+            if (rowLength != elementsArr[i].length)
+                return false;
+        }
+
+        return true;
+    }
+
+    //check if given array has same columns and rows size
+    private boolean hssEqualSize(T[][] arr)
+    {
+        //        if (arr.length != columns)
+        //            return false;
+        //        else if (arr[0].length != rows)
+        //            return false;
+        //        else
+        //            return true;
+
+        //assumes that given arr passed isMatrix
+        return (arr.length == columns) && (arr[0].length == rows);
+    }
+
+    //checks if adding given array to this.elementsArr is possible
+    private boolean mayAdd(T[][] arr)
+    {
+        return isMatrix(arr) && hssEqualSize(arr);
+    }
+
+    public T[][] addMatrix(T[][] arr)
+    {
+        //check given arr
+        if (!mayAdd(arr))
+            return null;
+
+
+        for (int i = 0; i < columns; i++)
+        {
+            //todo check this warning
+            for (int j = 0; j < arr[i].length; j++)
+                arr[i][j] = (T) Double.valueOf(arr[i][j].doubleValue() + elementsArr[i][j].doubleValue()); //For starters cast double to Double then to T that extends Number
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args)
+    {
+    }
+}
